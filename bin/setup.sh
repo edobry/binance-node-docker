2163b3e638796d3e4680e88e94e8b81e22b01ab9
Eugene Dobry <edobry@gmail.com>
2020-03-23 16:59:17 -0400

    disable config editing

--- bin/setup.sh
+++ bin/setup.sh
@@ -18,7 +18,3 @@ else
     ln -sf /node-binary/cli/${BNET}/${BVER}/linux/bnbcli /usr/local/bin/bnbcli
     chmod +x /usr/local/bin/bnbcli
 fi
-
-# Turn on console logging
-
-sed -i 's/logToConsole = false/logToConsole = true/g' ${BNCHOME}/config/app.toml
